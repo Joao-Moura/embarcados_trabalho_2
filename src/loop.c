57188e591de28ca98a3e518821cfa5d872f823f1
Joao-Moura <jaummoura15@gmail.com>
2023-01-22 18:41:08 -0300

    Melhorando a estrutura do código

---
+++ src/loop.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "modbus.h"
+#include "uart.h"
+#include "bme280.h"
+#include "gpio.h"
+#include "vars.h"
+#include "utils.h"
+#include "pid.h"
+
+void gerencia_forno (int dado) {
+    dado &= 0x1;
+    monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD3, (void *)&dado, 1);
+    le_msg(uart0_fd, buffer_escrita, (void *)&dado);
+    estado_atual.ligado = dado;
+
+    if (!estado_atual.ligado) {
+        estado_atual.em_funcionamento = 0;
+        monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD5, (void *)&estado_atual.em_funcionamento, 1);
+        le_msg(uart0_fd, buffer_escrita, (void *)&dado);
+    }
+}
+
+void gerencia_funcionamento (int dado) {
+    dado &= 0x1;
+    monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD5, (void *)&dado, 1);
+    le_msg(uart0_fd, buffer_escrita, (void *)&dado);
+    estado_atual.em_funcionamento = dado;
+}
+
+void gerencia_modo (int dado) {
+    estado_atual.modo_controle ^= 1;
+    estado_curva = 0;
+    monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD4, (void *)&estado_atual.modo_controle, 1);
+    le_msg(uart0_fd, buffer_escrita, (void *)&dado);
+}
+
+void gerencia_temperaturas () {
+    monta_msg(buffer_envio, &tamanho_mensagem, 0x23, 0xC1, NULL, 0);
+    le_msg(uart0_fd, buffer_escrita, (void *)&estado_atual.temperatura_interna);
+
+    if (!estado_atual.modo_controle || (!estado_atual.em_funcionamento && estado_atual.modo_controle)) {
+        monta_msg(buffer_envio, &tamanho_mensagem, 0x23, 0xC2, NULL, 0);
+        le_msg(uart0_fd, buffer_escrita, (void *)&estado_atual.temperatura_referencia);
+    } else {
+        estado_atual.temperatura_referencia = temp_curva;
+    }
+
+    printf("Temperaturas interna: %.2f e de referencia: %.2f\n", estado_atual.temperatura_interna, estado_atual.temperatura_referencia); 
+
+    if (estado_atual.em_funcionamento) {
+        pid_atualiza_referencia(estado_atual.temperatura_referencia);
+        int potencia = (int) pid_controle(estado_atual.temperatura_interna);
+        printf("=============== NOVA POTENCIA = %d =========\n", potencia);
+        set_controle(potencia);
+    }
+}
+
+void loop_principal () {
+    while(1) {
+        monta_msg(buffer_envio, &tamanho_mensagem, 0x23, 0xC3, NULL, 0);
+
+        if (uart0_fd != -1) {
+            int dado = 0;
+            int tamanho_buffer = le_msg(uart0_fd, buffer_escrita, (void *)&dado);
+
+            if (tamanho_buffer > 0) {
+                printf("Dado lido: %d\n", dado);
+                if ((dado == 161) || (dado == 162))
+                    gerencia_forno(dado);
+                else if (((dado == 163) || (dado == 164)) & estado_atual.ligado)
+                    gerencia_funcionamento(dado);
+                else if ((dado == 165) && estado_atual.ligado)
+                    gerencia_modo(dado);
+                else
+                    gerencia_temperaturas();
+            } else {
+                usleep(250000);
+            }
+        }
+    }
+}
--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 #include "vars.h"
 #include "utils.h"
 #include "pid.h"
+#include "loop.h"
 
 
 void finaliza_programa (int sinal) {
@@ -39,62 +40,6 @@ void init() {
 int main (int argc, char *argv[]) {
     signal(SIGINT, finaliza_programa);
     init();
-
-    while(1) {
-        monta_msg(buffer_envio, &tamanho_mensagem, 0x23, 0xC3, NULL, 0);
-
-        if (uart0_fd != -1) {
-            int dado = 0;
-            int tamanho_buffer = le_msg(uart0_fd, buffer_escrita, (void *)&dado);
-
-            if (tamanho_buffer > 0) {
-                printf("Dado lido: %d\n", dado);
-                if ((dado == 161) || (dado == 162)) {
-                    dado &= 0x1;
-                    monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD3, (void *)&dado, 1);
-                    le_msg(uart0_fd, buffer_escrita, (void *)&dado);
-                    estado_atual.ligado = dado;
-
-                    if (!estado_atual.ligado) {
-                        estado_atual.em_funcionamento = 0;
-                        monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD5, (void *)&estado_atual.em_funcionamento, 1);
-                        le_msg(uart0_fd, buffer_escrita, (void *)&dado);
-                    }
-                } else if (((dado == 163) || (dado == 164)) & estado_atual.ligado) {
-                    dado &= 0x1;
-                    monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD5, (void *)&dado, 1);
-                    le_msg(uart0_fd, buffer_escrita, (void *)&dado);
-                    estado_atual.em_funcionamento = dado;
-                } else if ((dado == 165) && estado_atual.ligado) {
-                    estado_atual.modo_controle ^= 1;
-                    estado_curva = 0;
-                    monta_msg(buffer_envio, &tamanho_mensagem, 0x16, 0xD4, (void *)&estado_atual.modo_controle, 1);
-                    le_msg(uart0_fd, buffer_escrita, (void *)&dado);
-                } else {
-                    monta_msg(buffer_envio, &tamanho_mensagem, 0x23, 0xC1, NULL, 0);
-                    le_msg(uart0_fd, buffer_escrita, (void *)&estado_atual.temperatura_interna);
-
-                    if (!estado_atual.modo_controle || (!estado_atual.em_funcionamento && estado_atual.modo_controle)) {
-                        monta_msg(buffer_envio, &tamanho_mensagem, 0x23, 0xC2, NULL, 0);
-                        le_msg(uart0_fd, buffer_escrita, (void *)&estado_atual.temperatura_referencia);
-                    } else {
-                        estado_atual.temperatura_referencia = temp_curva;
-                    }
-
-                    printf("Temperaturas interna: %.2f e de referencia: %.2f\n", estado_atual.temperatura_interna, estado_atual.temperatura_referencia); 
-
-                    if (estado_atual.em_funcionamento) {
-                        pid_atualiza_referencia(estado_atual.temperatura_referencia);
-                        int potencia = (int) pid_controle(estado_atual.temperatura_interna);
-                        printf("=============== NOVA POTENCIA = %d =========\n", potencia);
-                        set_controle(potencia);
-                    }
-                }
-            } else {
-                usleep(250000);
-            }
-        }
-    }
-
+    loop_principal();
     return 0;
 }
